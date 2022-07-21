1f8be004eb92c2e362f6dfa6c70667d4ba96dbe1
Quang Vu <ngqangvvu@gmail.com>
2022-07-21 17:10:57 +0700

    update image

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import Rating from './Rating';
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
-import '../styles/product.css';
+import React from "react";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
+import { Card, Image } from "react-bootstrap";
+import "../styles/product.css";
+import cart1 from "../assets/cartProduct.svg";
 
 const Product = ({ product }) => {
-	return (
-		<Card className='mt-3 p-0'>
-			<Link to={`/product/${product._id}`}>
-				<Card.Img
-					loading='lazy'
-					className='product-image'
-					src={product.image}
-					variant='top'
-					alt={product.name}
-				/>
-			</Link>
+  return (
+    <Card className="mt-3 p-0">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img
+          loading="lazy"
+          className="product-image"
+          src={product.image}
+          variant="top"
+          alt={product.name}
+        />
+      </Link>
 
-			<Card.Body>
-				<Link
-					to={`/product/${product._id}`}
-					style={{  textDecoration: 'none' }}>
-					<Card.Title className='product-title' as='body'>
-						{product.name}
-					</Card.Title>
-				</Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`} style={{ textDecoration: "none" }}>
+          <Card.Title className="product-title" as="body">
+            {product.name}
+          </Card.Title>
+        </Link>
 
-				<Card.Text as='div'>
+        {/* <Card.Text as='div'>
 					{product && product.rating && (
 						<Rating
 							value={product.rating}
@@ -35,19 +34,37 @@ const Product = ({ product }) => {
 							}`}
 						/>
 					)}
-				</Card.Text>
+				</Card.Text> */}
+        <div className="row justify-content-between	">
+          <div className="col-auto">
+            <Card.Text as="h4">
+              {product.price &&
+                product.price.toLocaleString("en-vn", {
+                  maximumFractionDigits: 2,
+                  style: "currency",
+                  currency: "vnd",
+                })}
+            </Card.Text>
+          </div>
 
-				<Card.Text as='h4'>
-					{product.price &&
-						product.price.toLocaleString('en-vn', {
-							maximumFractionDigits: 2,
-							style: 'currency',
-							currency: 'vnd',
-						})}
-				</Card.Text>
-			</Card.Body>
-		</Card>
-	);
+          <div className="col-auto align-items-end">
+            <Link to={`/product/${product._id}`}>
+              <Image
+                rounded
+                style={{
+                  marginTop: "10px",
+                  width: "19.71px",
+                  height: "24px",
+                  objectFit: "cover",
+                }}
+                src={cart1}
+              />
+            </Link>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Product;
