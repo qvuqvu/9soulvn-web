069fb4463b4ea37115dc4d4f53536dfad7099a6d
Quang Vu <ngqangvvu@gmail.com>
2022-08-10 12:40:22 +0700

    Fix introduce section

--- frontend/src/components/introduceHome.js
+++ frontend/src/components/introduceHome.js
@@ -4,11 +4,16 @@ const IntroduceHome = () => {
       <div className="row align-items-center">
         <div className="col align-self-center">
           <div className="h1 ">9soul.vn</div>
-          <div className="h2 mt-4">Thảnh thơi tìm tới</div>
+          <div className="h2 mt-4">thảnh thơi tìm tới</div>
           <hr width="100%" align="center" />
-          
-          <div className="mt-2">
-          Ở đây có một không gian thảnh thơi với những câu chuyện chân thành, những cảm xúc thật thà và những con chữ mộc mạc... để bạn lắng nghe, tin tưởng và thương mình nhiều hơn một chút...           </div>
+          <div className="mt-[5%] mb-[3%] text-[16px] text-White font-head">
+            Cảm ơn đã chọn 9soul như một trạm nạp cảm xúc của một đôi lần muốn
+            “thảnh thơi tìm tới” – MC’s
+          </div>
+          <hr width="100%" align="center" />
+          <div className="mt-5">
+          Ở đây có một không gian thảnh thơi với những câu chuyện chân thành, những cảm xúc thật thà và những con chữ mộc mạc... để bạn lắng nghe, tin tưởng và thương mình nhiều hơn một chút...
+          </div>
         </div>
 
         <div className="col">
