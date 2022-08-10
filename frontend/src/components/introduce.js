6beeff66816288ebacfd6259cb0914e8ebbf4ddd
Quang Vu <ngqangvvu@gmail.com>
2022-08-10 12:40:12 +0700

    Fix introduce section home

--- frontend/src/components/introduce.js
+++ frontend/src/components/introduce.js
@@ -4,18 +4,17 @@ const Introduce = () => {
       <div className="row align-items-center">
         <div className="col align-self-center">
           <div className="h1 ">9soul.vn / gifting</div>
-          <div className="h2 mt-4">A little thing</div>
+          <div className="h2 mt-4">gieo vài điều nhỏ xíu</div>
           <hr width="100%" align="center" />
           <div className="mt-[3%] mb-[3%] text-[16px] text-White font-head">
-            Cảm ơn đã chọn 9soul như một trạm nạp cảm xúc của một đôi lần muốn
-            “Thảnh thơi tìm tới” – MC’s
+            Trong những lúc buồn bã nhất của cuộc đời vốn dĩ nhiều lắng lo,
+            những món quả nhỏ lại gieo hy vọng to… – MC’s
           </div>
           <hr width="100%" align="center" />
           <div className="mt-5">
-            Chúng mình tin rằng, rồi sau cùng của cuộc đời, chúng ta cũng chỉ
-            mong có thể gói gém mấy chục năm khôn lớn lại thành một câu chuyện
-            đáng để nghe. Và trong một khoảnh khắc định mệnh nào đó, câu chuyện
-            ấy chính là cảm hứng.
+            Một món quà, có thể là “cũ” vật nhưng cũng có thể là kỉ vật, nơi cất
+            giữ một phần rất đẹp của tâm hồn người ấy. Người đã từng vì cảm mến
+            mà thương tặng mình. Ở đây có những món đồ dự sẽ trở thành kỉ vật.
           </div>
         </div>
 
